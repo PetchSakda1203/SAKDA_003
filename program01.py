26d7894c1a8a8718eb1605ebcc992560d047ecd9
PetchSakda1203 <164302@wsk.ac.th>
2026-06-17 03:17:34 +0000

    program02.py

---
+++ program01.py
@@ -0,0 +1,4 @@
+print("sawaddee")
+print("............")
+num1=float(input("enter number1:"))
+num2=int(input("enter number2:"))
---
+++ program02.py
@@ -0,0 +1,7 @@
+print("โปรแกรมคำนวณหาพื้นที่สามเหลี่ยม")
+print("---------------------------")
+tall = float(input("enter hieght:"))
+base = float(input("enter base:"))
+areatri = 0.5*tall*base
+print("พื้นที่สามเหลี่ยม =",areatri)
+print("ผู้พัฒนาโปรแกรม โดย ศักดิ์ดา")
